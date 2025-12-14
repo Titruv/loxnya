4862296bdd2e725985806ffcd4fcce91bb2042c9
Titruv <daratitovec82@gmail.com>
2025-12-15 00:17:26 +0300

    first commit

---
+++ BasicDataTypes/Program.cs
@@ -0,0 +1,9 @@
+﻿  class Program
+{
+ static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var result = numbers.Where(n => n % 2 == 0).ToArray();
+            Console.WriteLine($"Task 1: {string.Join(", ", result)}");
+        }
+}
