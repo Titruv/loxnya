38726c905ef43dbc2eaa24d6e2a9f363e0269752
Titruv <daratitovec82@gmail.com>
2025-12-15 00:22:10 +0300

    2 commit

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -1,6 +1,6 @@
 ﻿  class Program
 {
- static void Main()
+ static void Main() //2 commit
         {
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var result = numbers.Where(n => n % 2 == 0).ToArray();
